d6237b042ad82c0134cf9e81fe5162057e54d66b
Raindrop Launcher. <kendrickpup@gmail.com>
2026-03-25 10:52:58 -0600

    Create color.kt

---
+++ color.kt
@@ -0,0 +1,43 @@
+package com.example.raindroplauncher.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Vibrant Comic-Inspired Palette
+val VibrantBlue = Color(0xFF00B0FF)
+val ElectricCyan = Color(0xFF00E5FF)
+val DeepPurple = Color(0xFF6200EA)
+val HotPink = Color(0xFFFF4081)
+val BrightOrange = Color(0xFFFF6D00)
+val NeonYellow = Color(0xFFFFD600)
+
+val DarkBlue = Color(0xFF0D47A1)
+val DarkBackground = Color(0xFF121212)
+val LightBackground = Color(0xFFF5F5F5)
+
+// Light Scheme Colors
+val md_theme_light_primary = Color(0xFF006495)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFCBE6FF)
+val md_theme_light_onPrimaryContainer = Color(0xFF001E30)
+val md_theme_light_secondary = Color(0xFF50606F)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFD3E5F6)
+val md_theme_light_onSecondaryContainer = Color(0xFF0C1D29)
+val md_theme_light_tertiary = Color(0xFF65587E)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFEBDCFF)
+val md_theme_light_onTertiaryContainer = Color(0xFF201637)
+
+// Dark Scheme Colors
+val md_theme_dark_primary = Color(0xFF8FCDFF)
+val md_theme_dark_onPrimary = Color(0xFF003450)
+val md_theme_dark_primaryContainer = Color(0xFF004B71)
+val md_theme_dark_onPrimaryContainer = Color(0xFFCBE6FF)
+val md_theme_dark_secondary = Color(0xFFB8C9D9)
+val md_theme_dark_onSecondary = Color(0xFF22323F)
+val md_theme_dark_secondaryContainer = Color(0xFF394856)
+val md_theme_dark_onSecondaryContainer = Color(0xFFD3E5F6)
+val md_theme_dark_tertiary = Color(0xFFCFC0E8)
+val md_theme_dark_onTertiary = Color(0xFF362B4D)
+val md_theme_dark_tertiaryContainer = Color(0xFF4D4165)
+val md_theme_dark_onTertiaryContainer = Color(0xFFEBDCFF)
